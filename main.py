1fd4a0f3d8a394b137583eb011516691b3b03a55
jcbondsii <jcbondsii@gmail.com>
2026-01-14 19:08:02 -0600

    Initial AI call

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    raise ValueError("GEMINI_API_KEY is not set in the environment variables.")
+
+client = genai.Client(api_key = api_key)
+
+model = "gemini-2.5-flash"
+contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
 def main():
-    print("Hello from aiagent!")
+    print(client.models.generate_content(model=model, contents=contents).text)
 
 
 if __name__ == "__main__":
