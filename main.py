ae4bb15ac7d758a5c4ddb1ba840041ded66b000b
jcbondsii <jcbondsii@gmail.com>
2026-01-17 11:11:41 -0600

    Added a verbose argument.

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ client = genai.Client(api_key = api_key)
 model = "gemini-2.5-flash"
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type = str, help="User prompt")
+#argument to enable verbose output
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
 
 #used for single prompt input
 contents = args.user_prompt
@@ -27,7 +30,10 @@ x = cmg_c.usage_metadata.prompt_token_count
 y = cmg_c.usage_metadata.candidates_token_count
 
 def main():
-    print(f"Contents: {contents}\nPrompt tokens: {x}\nResponse tokens: {y}\nResponse: {text}")
+    if args.verbose:
+        print(f"User prompt: {contents}\nPrompt tokens: {x}\nResponse tokens: {y}\nResponse: {text}")
+    else:
+        print(f"Response: {text}")
 
 
 if __name__ == "__main__":
