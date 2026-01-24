1ff900a6f7bc8ace2dc299dba4533f34271d2332
jcbondsii <jcbondsii@gmail.com>
2026-01-24 10:58:10 -0600

    Added the ability to read functions.

--- main.py
+++ main.py
@@ -1,40 +1,55 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-import argparse
+
 from prompts import system_prompt
+from functions.call_function import available_functions
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if not api_key:
-    raise ValueError("GEMINI_API_KEY is not set in the environment variables.")
-
-client = genai.Client(api_key = api_key)
-
-model = "gemini-2.5-flash"
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type = str, help="User prompt")
-#argument to enable verbose output
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
-
-
-#used for single prompt input
-contents = args.user_prompt
-#used for multiple prompt input
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
-cmg_c = client.models.generate_content(model=model, contents=messages, config = types.GenerateContentConfig(system_instruction=system_prompt, temperature = 0))
-text = cmg_c.text
-x = cmg_c.usage_metadata.prompt_token_count
-y = cmg_c.usage_metadata.candidates_token_count
 
 def main():
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     if args.verbose:
-        print(f"User prompt: {contents}\nPrompt tokens: {x}\nResponse tokens: {y}\nResponse: {text}")
+        print(f"User prompt: {args.user_prompt}\n")
+
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+        ),
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
     else:
-        print(f"Response: {text}")
+        print(response.text)
 
 
 if __name__ == "__main__":
